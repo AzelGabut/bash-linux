145c5382031cafec398158068826704bcd478224
xtsea <90479255+xtsea@users.noreply.github.com>
2022-12-31 18:20:24 +0700

    fixed

--- base.sh
+++ base.sh
@@ -4,12 +4,13 @@ sudo apt update -y && sudo apt upgrade -y && sudo apt-get install -y python3-pip
 clear
 git clone -b dev https://github.com/TeamKillerX/KillerX-Base && cd KillerX-Base && pip3 install --no-cache-dir -r requirements.txt
 echo """
-API_ID=1234
-API_HASH=akska
+API_ID=
+API_HASH=
+OWNER_ID=
 BOT_TOKEN=
-ALIVE_PIC=
-LOG_GROUP=
 PACK_NAME=kang pack
+LOG_GROUP=
+ALIVE_PIC=
 STRING_SESSION1=
 """ > config.env
 clear
