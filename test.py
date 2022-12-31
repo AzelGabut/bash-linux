1c280a6b9c61489eb45be0e1d540da4af87e6b8d
xtsea <90479255+xtsea@users.noreply.github.com>
2022-12-31 14:57:29 +0700

    _

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/bin/bash
+import os
+
+print ("\033[1;32mAUTHOR :  \033[1;34mDeleveoper By Randy")
+print ("\033[1;33mTelegram : @xtsea")
+print ("\033[1;34mINSTALL KILLERXBASE USERBOT")
+
+os.system("git clone -b dev https://github.com/TeamKillerX/KillerX-Base && cd KillerX-Base")
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clear
-git clone -b dev https://github.com/TeamKillerX/KillerX-Base
-mv -f KillerX-Base /sdcard
-rm -rf test.sh
