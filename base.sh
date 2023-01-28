9d3daa856adb7a6bdf2bb06f56aac3998b9ce1bb
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-28 17:46:22 +0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,13 +2,13 @@
 
 sudo apt update -y && sudo apt upgrade -y && sudo apt-get install -y python3-pip ffmpeg npm curl git wget neofetch screen
 clear
-git clone -b dev https://github.com/TeamKillerX/KillerX-Base && cd KillerX-Base && pip3 install --no-cache-dir -r requirements.txt
+git clone -b dev https://github.com/TeamKillerX/DarkWeb && cd DarkWeb && pip3 install --no-cache-dir -r requirements.txt
 read -p "Enter Api id: " a 
 read -p "Enter Api Hash: " b
 read -p "Enter Owner id: " c
 read -p "Enter Bot token: " d
 read -p "Enter Alive Pic: " h 
-read -p "Enter Log Group: " y
+read -p "Enter Openai api: " v
 read -p "Enter string pyrogram: " f
 green='\033[0;32m'
 clear='\033[0m'
@@ -18,7 +18,7 @@ API_HASH= $b
 OWNER_ID= $c
 BOT_TOKEN= $d
 ALIVE_PIC= $h
-LOG_GROUP= $y
+OPENAI_API= $v
 STRING_SESSION1= $f
 PACK_NAME=kang pack
 GCAST_BLACKLIST=-1001704645461
